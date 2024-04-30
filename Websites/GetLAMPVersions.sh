f72374214ac65dcb58d85b7e2ee446334c07be8f
Madeleine Rabitoy <rabitoy@hope.edu>
2024-04-30 16:04:22 -0400

    Update GetLAMPVersions.sh

--- Websites/GetLAMPVersions.sh
+++ Websites/GetLAMPVersions.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-apache2 -v
-httpd -v
-mysql -V
-nginx -v
-php -v
+apache2 -v 2> /dev/null
+httpd -v 2> /dev/null
+mysql -V 2> /dev/null
+nginx -v 2> /dev/null
+php -v 2> /dev/null
+
+echo "[Success] Retrieved all possible LAMP version information."
