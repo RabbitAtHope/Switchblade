92183e8413b804efe176b437a595d4a5bce43a7d
Madeleine Rabitoy <rabitoy@hope.edu>
2024-04-30 16:00:21 -0400

    Update RestartWebServices.sh

--- Websites/RestartWebServices.sh
+++ Websites/RestartWebServices.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Apache2
-sudo systemctl restart apache2
+sudo systemctl restart apache2 2> /dev/null
 
 # Httpd
-sudo systemctl restart httpd
-sudo systemctl restart httpd24-httpd
+sudo systemctl restart httpd 2> /dev/null
+sudo systemctl restart httpd24-httpd 2> /dev/null
 
 # Tomcat
-sudo systemctl restart tomcat
+sudo systemctl restart tomcat 2> /dev/null
+
+echo "[Success] Restarted all possible web services."
