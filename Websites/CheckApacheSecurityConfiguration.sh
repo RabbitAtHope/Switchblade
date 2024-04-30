648a4890c137facaab42ec91bb6a09a219758462
Madeleine Rabitoy <rabitoy@hope.edu>
2024-04-30 16:09:56 -0400

    Update CheckApacheSecurityConfiguration.sh

--- Websites/CheckApacheSecurityConfiguration.sh
+++ Websites/CheckApacheSecurityConfiguration.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # ServerSignature
+echo "Checking the [ServerSignature] setting, which should be set to [Off]..."
 cat /etc/apache2/conf-enabled/security.conf | grep "ServerSignature" | grep -v "#ServerSignature"  | grep -v "# ServerSignature"
+
 # ServerTokens
+echo "Checking the [ServerTokens] setting, which should be set to [Prod]..."
 cat /etc/apache2/conf-enabled/security.conf | grep "ServerTokens" | grep -v "#ServerTokens"  | grep -v "# ServerTokens"
