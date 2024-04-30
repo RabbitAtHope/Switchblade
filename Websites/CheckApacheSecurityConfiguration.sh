7161b6502cb82ac1bb36dbaa3c27abd58b86fba4
Madeleine Rabitoy <rabitoy@hope.edu>
2024-04-30 13:13:24 -0400

    Fixed Unix line endings

--- Websites/CheckApacheSecurityConfiguration.sh
+++ Websites/CheckApacheSecurityConfiguration.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
+# ServerSignature
 cat /etc/apache2/conf-enabled/security.conf | grep "ServerSignature" | grep -v "#ServerSignature"  | grep -v "# ServerSignature"
+# ServerTokens
 cat /etc/apache2/conf-enabled/security.conf | grep "ServerTokens" | grep -v "#ServerTokens"  | grep -v "# ServerTokens"
