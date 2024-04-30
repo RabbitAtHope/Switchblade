cde9c145ca2dd1f1221b7595bf1ef1caa34cabf6
Madeleine Rabitoy <rabitoy@hope.edu>
2024-04-30 16:07:06 -0400

    Update RestartPapercut.sh

--- Printing/RestartPapercut.sh
+++ Printing/RestartPapercut.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo systemctl restart pc-app-server 2> /dev/null
 
-echo "[Success] Restarted the PaperCut web app service, if present."
+echo "[Success] Restarted the PaperCut web app printing service, if present."
