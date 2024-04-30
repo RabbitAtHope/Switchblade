0682acbbcbc1af94c2d5b2d7d9b8f777b499ce3b
Madeleine Rabitoy <rabitoy@hope.edu>
2024-04-30 16:15:36 -0400

    Update CheckApacheSecurityConfiguration.sh

--- Websites/CheckApacheSecurityConfiguration.sh
+++ Websites/CheckApacheSecurityConfiguration.sh
@@ -2,8 +2,10 @@
 
 # ServerSignature
 echo "Checking the [ServerSignature] setting, which should be set to [Off]..."
-cat /etc/apache2/conf-enabled/security.conf | grep "ServerSignature" | grep -v "#ServerSignature"  | grep -v "# ServerSignature"
+cat /etc/apache2/conf-enabled/security.conf 2>/dev/null | grep "ServerSignature" | grep -v "#ServerSignature"  | grep -v "# ServerSignature"
+cat /etc/httpd/conf-enabled/security.conf 2>/dev/null | grep "ServerSignature" | grep -v "#ServerSignature"  | grep -v "# ServerSignature"
 
 # ServerTokens
 echo "Checking the [ServerTokens] setting, which should be set to [Prod]..."
-cat /etc/apache2/conf-enabled/security.conf | grep "ServerTokens" | grep -v "#ServerTokens"  | grep -v "# ServerTokens"
+cat /etc/apache2/conf-enabled/security.conf 2>/dev/null | grep "ServerTokens" | grep -v "#ServerTokens"  | grep -v "# ServerTokens"
+cat /etc/httpd/conf-enabled/security.conf 2>/dev/null | grep "ServerTokens" | grep -v "#ServerTokens"  | grep -v "# ServerTokens"
