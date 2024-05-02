02cc6183a8844664820ae5177a1036df3734a219
Madeleine Rabitoy <rabitoy@hope.edu>
2024-05-02 09:15:20 -0400

    Added nginx

--- Websites/RestartWebServices.sh
+++ Websites/RestartWebServices.sh
@@ -7,6 +7,9 @@ sudo systemctl restart apache2 2> /dev/null
 sudo systemctl restart httpd 2> /dev/null
 sudo systemctl restart httpd24-httpd 2> /dev/null
 
+# Nginx
+sudo systemctl restart nginx 2> /dev/null
+
 # Tomcat
 sudo systemctl restart tomcat 2> /dev/null
 
