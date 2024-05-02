7c9d66cdb8a6d2d85df3e05be85e8db94326ade4
Madeleine Rabitoy <rabitoy@hope.edu>
2024-05-02 09:30:18 -0400

    Made output more visually pleasing

--- Websites/CheckApacheSecurityConfiguration.sh
+++ Websites/CheckApacheSecurityConfiguration.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
+echo ""
+
+#//////////////////////////
+
 # ServerSignature
 echo "Checking the [ServerSignature] setting, which should be set to [Off]..."
+
+cat /etc/apache2/apache2.conf 2>/dev/null | grep "ServerSignature" | grep -v "#ServerSignature"  | grep -v "# ServerSignature"
 cat /etc/apache2/conf-enabled/security.conf 2>/dev/null | grep "ServerSignature" | grep -v "#ServerSignature"  | grep -v "# ServerSignature"
 cat /etc/httpd/conf-enabled/security.conf 2>/dev/null | grep "ServerSignature" | grep -v "#ServerSignature"  | grep -v "# ServerSignature"
+cat /etc/httpd/httpd.conf 2>/dev/null | grep "ServerSignature" | grep -v "#ServerSignature"  | grep -v "# ServerSignature"
+
+echo ""
+
+#//////////////////////////
 
 # ServerTokens
 echo "Checking the [ServerTokens] setting, which should be set to [Prod]..."
+
+cat /etc/apache2/apache2.conf 2>/dev/null | grep "ServerTokens" | grep -v "#ServerTokens"  | grep -v "# ServerTokens"
 cat /etc/apache2/conf-enabled/security.conf 2>/dev/null | grep "ServerTokens" | grep -v "#ServerTokens"  | grep -v "# ServerTokens"
 cat /etc/httpd/conf-enabled/security.conf 2>/dev/null | grep "ServerTokens" | grep -v "#ServerTokens"  | grep -v "# ServerTokens"
+cat /etc/httpd/httpd.conf 2>/dev/null | grep "ServerTokens" | grep -v "#ServerTokens"  | grep -v "# ServerTokens"
+
+echo ""
+
+#//////////////////////////
+
+echo "[Success] Checked all possible configuration locations."
