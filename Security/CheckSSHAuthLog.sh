6519bfe5fd456bee4c9a7b80f41fcf5b8ff3b61d
Madeleine Rabitoy <rabitoy@hope.edu>
2024-05-02 09:54:06 -0400

    Added new security scripts

---
+++ Security/CheckSSHAuthLog.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo ""
+
+#//////////////////////////
+
+echo "Fetching all [successful recent logins]..."
+
+# Get all recent logins and the IP addresses they came from.
+sudo cat /var/log/auth.log 2> /dev/null | grep "Accepted password"
+
+echo ""
+
+#//////////////////////////
+
+echo "Fetching recent [failed attempts] to [existing accounts] that aren't [root]..."
+
+# Get all recent failed login attempts to accounts that exist.
+sudo cat /var/log/auth.log 2> /dev/null | grep "Failed password for" | grep -v "Failed password for invalid user" | grep -v "Failed password for root"
+
+echo ""
+
+#//////////////////////////
+
+echo "Fetching recent [failed attempts] to [nonexistent accounts], which are likely brute force attempts..."
+
+# Get all recent failed login attempts to accounts that DON'T exist.
+sudo cat /var/log/auth.log 2> /dev/null | grep "Failed password for invalid user"
+
+echo ""
+
+#//////////////////////////
+
+echo "Fetching recent [failed attempts] to log into [root], which are likely malicious attempts..."
+
+# Get all recent failed login attempts to the root account.
+sudo cat /var/log/auth.log 2> /dev/null | grep "Failed password for root"
+
+echo ""
+
+#//////////////////////////
