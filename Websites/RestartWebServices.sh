afa3b8a8b8c4bf7fb1441309edeb2d582551d1bb
Madeleine Rabitoy <rabitoy@hope.edu>
2024-05-02 09:18:31 -0400

    Added more common web services

--- Websites/RestartWebServices.sh
+++ Websites/RestartWebServices.sh
@@ -3,9 +3,21 @@
 # Apache2
 sudo systemctl restart apache2 2> /dev/null
 
+# Caddy
+sudo systemctl restart caddy 2> /dev/null
+
+# Cherokee
+sudo systemctl restart cherokee 2> /dev/null
+
 # Httpd
 sudo systemctl restart httpd 2> /dev/null
 sudo systemctl restart httpd24-httpd 2> /dev/null
+
+# Jetty
+sudo systemctl restart jetty 2> /dev/null
+
+# Lighttpd
+sudo systemctl restart lighttpd 2> /dev/null
 
 # Nginx
 sudo systemctl restart nginx 2> /dev/null
