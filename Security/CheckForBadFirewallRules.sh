cbd72857272a7763fb9d220a38238799bea3519b
Madeleine Rabitoy <rabitoy@hope.edu>
2024-05-02 10:11:52 -0400

    Update CheckForBadFirewallRules.sh

--- Security/CheckForBadFirewallRules.sh
+++ Security/CheckForBadFirewallRules.sh
@@ -1,1 +1,35 @@
 #!/bin/bash
+
+echo ""
+
+#//////////////////////////
+
+# Port 21 (FTP)
+echo "Checking for overly permissive [FTP]/[21] rules..."
+
+# Firewalld
+# Shorewall
+# sudo cat /etc/shorewall/rules | grep...
+# UFW
+sudo ufw status | grep "21 " 2> /dev/null | grep "ALLOW" | grep "Anywhere"
+sudo ufw status | grep "21/tcp " 2> /dev/null | grep "ALLOW" | grep "Anywhere"
+sudo ufw status | grep "FTP " 2> /dev/null | grep "ALLOW" | grep "Anywhere"
+
+echo ""
+
+#//////////////////////////
+
+# Port 22 (SSH)
+echo "Checking for overly permissive [SSH]/[22] rules..."
+
+# Firewalld
+# Shorewall
+# sudo cat /etc/shorewall/rules | grep...
+# UFW
+sudo ufw status | grep "22 " 2> /dev/null | grep "ALLOW" | grep "Anywhere"
+sudo ufw status | grep "22/tcp " 2> /dev/null | grep "ALLOW" | grep "Anywhere"
+sudo ufw status | grep "SSH " 2> /dev/null | grep "ALLOW" | grep "Anywhere"
+
+echo ""
+
+#//////////////////////////
