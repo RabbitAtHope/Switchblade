43c9abe776344028e7e9855de45ea792114a1193
Madeleine Rabitoy <rabitoy@hope.edu>
2024-05-02 13:24:46 -0400

    Added more .git file types

--- Filesystem/FindAllGitFiles.sh
+++ Filesystem/FindAllGitFiles.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
 
+echo ""
+
+#//////////////////////////
+
+echo "Finding all [.git] files..."
+
 sudo find / -type f -iname .git
+
+echo ""
+
+#//////////////////////////
+
+echo "Finding all [.gitattributes] files..."
+
+sudo find / -type f -iname .gitattributes
+
+echo ""
+
+#//////////////////////////
+
+echo "Finding all [.github] files..."
+
+sudo find / -type f -iname .github
+
+echo ""
+
+#//////////////////////////
+
+echo "Finding all [.gitignore] files..."
+
+sudo find / -type f -iname .gitignore
+
+echo ""
+
+#//////////////////////////
