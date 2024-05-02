06b5c5ceafa8a2506b5623e74f0a2292413ade30
Madeleine Rabitoy <rabitoy@hope.edu>
2024-05-02 09:23:46 -0400

    Organized better

--- Websites/GetLAMPVersions.sh
+++ Websites/GetLAMPVersions.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
+echo "Checking [web server] versions..."
+
 # Checking web servers
 apache2 -v 2> /dev/null
 httpd -v 2> /dev/null
 nginx -v 2> /dev/null
 lighttpd -v 2> /dev/null
 
+#//////////////////////////
+
+echo "Checking [database] versions..."
+
 # Checking databases
-mysql -V 2> /dev/null
 mariadb -V 2> /dev/null
+mongod --version 2> /dev/null
+mysql -V 2> /dev/null
 percona -V 2> /dev/null
 psql --version 2> /dev/null
 redis-server --version 2> /dev/null
-mongod --version 2> /dev/null
+
+#//////////////////////////
+
+echo "Checking [programming language] versions..."
 
 # Checking programming languages
 php -v 2> /dev/null
 python --version 2> /dev/null
 python3 --version 2> /dev/null
 ruby -v 2> /dev/null
+
+#//////////////////////////
 
 echo "[Success] Retrieved all possible LAMP version information."
