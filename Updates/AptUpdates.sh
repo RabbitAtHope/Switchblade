0b060ad78c6a500278b5eca9a92fe6f9b83bd1fe
Madeleine Rabitoy <rabitoy@hope.edu>
2024-05-02 09:33:05 -0400

    Cleaned up output

--- Updates/AptUpdates.sh
+++ Updates/AptUpdates.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+echo ""
+
+#//////////////////////////
+
+echo "[Installing] updates..."
+
 # Perform the updates
 sudo apt-get update
 sudo apt-get upgrade -y
 
+#//////////////////////////
+
+echo "[Cleaning up] after updates..."
+
 # Clean up
 sudo apt-get autoclean
 sudo apt-get autoremove -y
+
+#//////////////////////////
 
 echo "[Success] Updates installed."
