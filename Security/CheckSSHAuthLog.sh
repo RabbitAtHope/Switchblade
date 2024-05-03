ad3ad344c1954eec5abacf33fa25f588971b1b66
Madeleine Rabitoy <rabitoy@hope.edu>
2024-05-03 09:27:10 -0400

    Added sort for better viewing

--- Security/CheckSSHAuthLog.sh
+++ Security/CheckSSHAuthLog.sh
@@ -7,7 +7,7 @@ echo ""
 echo "Fetching all [successful recent logins]..."
 
 # Get all recent logins and the IP addresses they came from.
-sudo cat /var/log/auth.log 2> /dev/null | grep "Accepted password"
+sudo cat /var/log/auth.log 2> /dev/null | grep "Accepted password" | sort
 
 echo ""
 
@@ -16,7 +16,7 @@ echo ""
 echo "Fetching recent [failed attempts] to [existing accounts] that aren't [root]..."
 
 # Get all recent failed login attempts to accounts that exist.
-sudo cat /var/log/auth.log 2> /dev/null | grep "Failed password for" | grep -v "Failed password for invalid user" | grep -v "Failed password for root"
+sudo cat /var/log/auth.log 2> /dev/null | grep "Failed password for" | grep -v "Failed password for invalid user" | grep -v "Failed password for root" | sort
 
 echo ""
 
@@ -25,7 +25,7 @@ echo ""
 echo "Fetching recent [failed attempts] to [nonexistent accounts], which are likely brute force attempts..."
 
 # Get all recent failed login attempts to accounts that DON'T exist.
-sudo cat /var/log/auth.log 2> /dev/null | grep "Failed password for invalid user"
+sudo cat /var/log/auth.log 2> /dev/null | grep "Failed password for invalid user" | sort
 
 echo ""
 
@@ -34,7 +34,7 @@ echo ""
 echo "Fetching recent [failed attempts] to log into [root], which are likely malicious attempts..."
 
 # Get all recent failed login attempts to the root account.
-sudo cat /var/log/auth.log 2> /dev/null | grep "Failed password for root"
+sudo cat /var/log/auth.log 2> /dev/null | grep "Failed password for root" | sort
 
 echo ""
 
