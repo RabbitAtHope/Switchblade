9dd921edc864d12d03d5f2d2f0f76251115f05ab
Madeleine Rabitoy <rabitoy@hope.edu>
2024-05-07 10:32:28 -0400

    Added pretty colors

--- Websites/CheckApacheSecurityConfiguration.sh
+++ Websites/CheckApacheSecurityConfiguration.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
+# Pretty colors.
+red='\033[0;31m'
+green='\033[0;32m'
+orange='\033[0;33m'
+blue='\033[0;34m'
+purple='\033[0;35m'
+cyan='\033[0;36m'
+lightgray='\033[0;37m'
+darkgray='\033[1;30m'
+lightred='\033[1;31m'
+lightgreen='\033[1;32m'
+yellow='\033[1;33m'
+lightblue='\033[1;34m'
+lightpurple='\033[1;35m'
+lightcyan='\033[1;36m'
+white='\033[1;37m'
+none='\033[0m'
+
 echo ""
 
 #//////////////////////////
 
 # ServerSignature
-echo "Checking the [ServerSignature] setting, which should be set to [Off]..."
+echo -e "Checking the [${yellow}ServerSignature${none}] setting, which should be set to [Off]..."
 
 cat /etc/apache2/apache2.conf 2>/dev/null | grep "ServerSignature" | grep -v "#ServerSignature"  | grep -v "# ServerSignature"
 cat /etc/apache2/conf-enabled/security.conf 2>/dev/null | grep "ServerSignature" | grep -v "#ServerSignature"  | grep -v "# ServerSignature"
@@ -17,7 +35,7 @@ echo ""
 #//////////////////////////
 
 # ServerTokens
-echo "Checking the [ServerTokens] setting, which should be set to [Prod]..."
+echo -e "Checking the [${yellow}ServerTokens${none}] setting, which should be set to [Prod]..."
 
 cat /etc/apache2/apache2.conf 2>/dev/null | grep "ServerTokens" | grep -v "#ServerTokens"  | grep -v "# ServerTokens"
 cat /etc/apache2/conf-enabled/security.conf 2>/dev/null | grep "ServerTokens" | grep -v "#ServerTokens"  | grep -v "# ServerTokens"
@@ -28,4 +46,4 @@ echo ""
 
 #//////////////////////////
 
-echo "[Success] Checked all possible configuration locations."
+echo -e "[${green}Success${none}] Checked all possible configuration locations."
