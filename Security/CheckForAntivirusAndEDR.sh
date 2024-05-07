bba587ae0cf57dcde8570db79781ee022f3c32ad
Madeleine Rabitoy <rabitoy@hope.edu>
2024-05-07 10:18:43 -0400

    Added pretty colors

--- Security/CheckForAntivirusAndEDR.sh
+++ Security/CheckForAntivirusAndEDR.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+# Pretty colors.
+red='\033[0;31m'
+green='\033[0;32m'
+orange='\033[0;33m'
+blue='\033[0;34m'
+purple='\033[0;35m'
+cyan='\033[0;36m'
+lightgray='\033[0;37m'
+darkgray='\033[1;30m'
+lightred='\033[1;31m'
+lightgreen='\033[1;32m'
+yellow='\033[1;33m'
+lightblue='\033[1;34m'
+lightpurple='\033[1;35m'
+lightcyan='\033[1;36m'
+white='\033[1;37m'
+none='\033[0m'
+
 echo ""
 
 #//////////////////////////
 
-echo "Checking for common [antivirus] services..."
+echo -e "[${green}Antivirus${none}] Checking for common [${green}antivirus${none}] services..."
 
 # Avast
 sudo systemctl status avast 2> /dev/null
@@ -25,7 +43,7 @@ echo ""
 
 #//////////////////////////
 
-echo "Checking for common [EDR] services..."
+echo -e "[${green}Endpoint Protection${none}] Checking for common [${green}EDR${none}] services..."
 
 # Carbon Black
 sudo systemctl status cbdaemon 2> /dev/null
