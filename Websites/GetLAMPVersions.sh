fc49187ac9775fa1a6a362ecbf3c2422f7680b31
Madeleine Rabitoy <rabitoy@hope.edu>
2024-05-07 10:37:37 -0400

    Made output prettier

--- Websites/GetLAMPVersions.sh
+++ Websites/GetLAMPVersions.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+# Pretty colors.
+red='\033[0;31m'
+green='\033[0;32m'
+orange='\033[0;33m'
+blue='\033[0;34m'
+purple='\033[0;35m'
+cyan='\033[0;36m'
+lightgray='\033[0;37m'
+darkgray='\033[1;30m'
+lightred='\033[1;31m'
+lightgreen='\033[1;32m'
+yellow='\033[1;33m'
+lightblue='\033[1;34m'
+lightpurple='\033[1;35m'
+lightcyan='\033[1;36m'
+white='\033[1;37m'
+none='\033[0m'
+
 echo ""
 
 #//////////////////////////
 
-echo "Checking [web server] versions..."
+echo -e "[${yellow}Web${none}] Checking [web server] versions..."
 
 # Checking web servers
 apache2 -v 2> /dev/null
@@ -16,7 +34,7 @@ echo ""
 
 #//////////////////////////
 
-echo "Checking [database] versions..."
+echo -e "[${yellow}Databases${none}] Checking [database] versions..."
 
 # Checking databases
 mariadb -V 2> /dev/null
@@ -30,7 +48,7 @@ echo ""
 
 #//////////////////////////
 
-echo "Checking [programming language] versions..."
+echo -e "[${yellow}Languages${none}] Checking [programming language] versions..."
 
 # Checking programming languages
 php -v 2> /dev/null
@@ -42,4 +60,4 @@ echo ""
 
 #//////////////////////////
 
-echo "[Success] Retrieved all possible LAMP version information."
+echo -e "[${green}Success${none}] Retrieved all possible LAMP version information."
