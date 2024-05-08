91c977df357448fab3c8f053f76381a9ec9e7360
Madeleine Rabitoy <rabitoy@hope.edu>
2024-05-08 14:23:56 -0400

    More breakdown of failure reasons

--- Monitoring/MonitorPostfixMail.sh
+++ Monitoring/MonitorPostfixMail.sh
@@ -32,6 +32,7 @@ dropped_count=$(grep -E "status=(bounced|deferred|expired|rejected)" /var/log/ma
 
 # Break dropped email count down by the reason
 bounced_count=$(grep -E "status=(bounced)" /var/log/mail.log | wc -l)
+bounced_count_badreceiver=$(grep "status=bounced" /var/log/mail.log | grep "The email account that you tried to reach does not exist" | wc -l)
 deferred_count=$(grep -E "status=(deferred)" /var/log/mail.log | wc -l)
 deferred_count_hostnotfound=$(grep "status=deferred" /var/log/mail.log | grep "Host or domain name not found" | wc -l)
 deferred_count_ratelimitednoauth=$(grep "status=deferred" /var/log/mail.log | grep "This mail has been rate limited because it is unauthenticated" | wc -l)
@@ -47,6 +48,7 @@ else
     echo -e "Dropped emails: ${white}[${none}${red}${dropped_count}${none}${white}]${none}"
 	
 	echo -e " Bounced: ${white}[${none}${red}${bounced_count}${none}${white}]${none}"
+	echo -e "  - Bad recipient: ${white}[${none}${red}${bounced_count_badreceiver}${none}${white}]${none}"
 	echo -e " Deferred: ${white}[${none}${red}${deferred_count}${none}${white}]${none}"
 	echo -e "  - Host not found: ${white}[${none}${red}${deferred_count_hostnotfound}${none}${white}]${none}"
 	echo -e "  - Rate limited unauthenticated host: ${white}[${none}${red}${deferred_count_ratelimitednoauth}${none}${white}]${none}"
