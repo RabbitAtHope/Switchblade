91bd917b0396e472aab1cdd3bfa5eb1f166af33e
Madeleine Rabitoy <rabitoy@hope.edu>
2024-05-08 13:49:40 -0400

    Create MonitorPostfixMail.sh

---
+++ Monitoring/MonitorPostfixMail.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Pretty colors.
+red='\033[0;31m'
+green='\033[0;32m'
+orange='\033[0;33m'
+blue='\033[0;34m'
+purple='\033[0;35m'
+cyan='\033[0;36m'
+lightgray='\033[0;37m'
+darkgray='\033[1;30m'
+lightred='\033[1;31m'
+lightgreen='\033[1;32m'
+yellow='\033[1;33m'
+lightblue='\033[1;34m'
+lightpurple='\033[1;35m'
+lightcyan='\033[1;36m'
+white='\033[1;37m'
+none='\033[0m'
+
+echo ""
+
+#//////////////////////////
+# Dropped / Sent Percentage
+#//////////////////////////
+
+# Get count of sent emails
+sent_count=$(grep "status=sent" /var/log/mail.log | wc -l)
+
+# Get count of dropped emails
+dropped_count=$(grep -E "status=(bounced|deferred|expired|rejected)" /var/log/mail.log | wc -l)
+
+# Calculate the percentage of dropped mail
+if [ "$sent_count" -eq 0 ]; then
+    echo "No sent emails in the specified time period."
+else
+    drop_percentage=$(echo "scale=2; $dropped_count / $sent_count * 100" | bc)
+    echo "Sent emails: [${sent_count}]"
+    echo "Dropped emails: [${dropped_count}]"
+    echo "Percentage of dropped mail: [${drop_percentage}%]"
+fi
+
+#//////////////////////////
