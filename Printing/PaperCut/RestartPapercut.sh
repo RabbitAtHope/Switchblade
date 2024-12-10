d604d99bbd793f3e42cbc95b50511cb7d4dbc198
Madeleine Rabitoy <rabitoy@hope.edu>
2024-12-10 08:57:04 -0500

    Added colors

--- Printing/PaperCut/RestartPapercut.sh
+++ Printing/PaperCut/RestartPapercut.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+
+# Pretty colors.
+red='\033[0;31m'
+green='\033[0;32m'
+orange='\033[0;33m'
+blue='\033[0;34m'
+purple='\033[0;35m'
+cyan='\033[0;36m'
+lightgray='\033[0;37m'
+darkgray='\033[1;30m'
+lightred='\033[1;31m'
+lightgreen='\033[1;32m'
+yellow='\033[1;33m'
+lightblue='\033[1;34m'
+lightpurple='\033[1;35m'
+lightcyan='\033[1;36m'
+white='\033[1;37m'
+none='\033[0m'
+
+echo ""
+
+#//////////////////////////
+
+echo -e "[${green}Restarting${none}] the PaperCut web app printing service..."
+
 sudo systemctl restart pc-app-server 2> /dev/null
 
-echo "[Success] Restarted the PaperCut web app printing service, if present."
+echo -e "[${green}Restarted${none}] the PaperCut web app printing service."
