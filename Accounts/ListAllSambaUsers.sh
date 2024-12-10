aecd489e7de339d43a247aff8e7887dcd788a2e3
Madeleine Rabitoy <rabitoy@hope.edu>
2024-12-10 08:55:18 -0500

    Added colors

--- Accounts/ListAllSambaUsers.sh
+++ Accounts/ListAllSambaUsers.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
+# Pretty colors.
+red='\033[0;31m'
+green='\033[0;32m'
+orange='\033[0;33m'
+blue='\033[0;34m'
+purple='\033[0;35m'
+cyan='\033[0;36m'
+lightgray='\033[0;37m'
+darkgray='\033[1;30m'
+lightred='\033[1;31m'
+lightgreen='\033[1;32m'
+yellow='\033[1;33m'
+lightblue='\033[1;34m'
+lightpurple='\033[1;35m'
+lightcyan='\033[1;36m'
+white='\033[1;37m'
+none='\033[0m'
+
 echo ""
 
 #//////////////////////////
 
-echo "[Samba users] on this machine:"
+echo -e "[${green}Samba users${none}] on this machine:"
 
 sudo pdbedit -L -v | grep "Unix username:" | sort
 
